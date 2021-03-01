5d3dfeff444087989d96994df36c6f09b13bd547
Merethe Hansen <merethe_@mac.com>
2021-02-28 23:06:15 -0800

    completed Utilities section completed Copy Special exercise

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -15,9 +15,37 @@ import commands
 """Copy Special exercise
 """
 
-# +++your code here+++
-# Write functions and modify main() to call them
+def get_special_paths(dirs):
+  # part A
+  # for all dirs, get files in dir
+  # filter for those that are special
+  paths = []
+  for di in dirs:
+    filenames = os.listdir(di)
+    for name in filenames:
+      match = re.search(r'__\w+__', name)
+      if match:
+        paths.append(os.path.abspath(os.path.join(di, name)))
+  return paths
 
+
+def copy_to(paths, di):
+  # part b
+  if not os.path.exists(di):
+    os.makedirs(di)
+  for path in paths:
+    shutil.copy(path, di)
+
+def zip_to(paths, zipfile):
+  # part c
+  cmd = 'zip -j ' + zipfile
+  for path in paths:
+    cmd = cmd + ' ' + path.replace(' ', r'\ ')
+  print "running external command: " + cmd
+  (status, output) = commands.getstatusoutput(cmd)
+  if status:
+    sys.stderr.write(output)
+    sys.exit(status)
 
 
 def main():
@@ -48,8 +76,17 @@ def main():
     print "error: must specify one or more dirs"
     sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
+  paths = get_special_paths(args)
+
+  if todir != '':
+    copy_to(paths, todir)
+  if tozip != '':
+    zip_to(paths, tozip)
+  if todir == '' and tozip == '':
+    for path in paths:
+      print path
+
+
   
 if __name__ == "__main__":
   main()
